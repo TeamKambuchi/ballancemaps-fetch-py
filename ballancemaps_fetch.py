15eccbc3b948187ff50de840c1413f5039c66416
Xenapte <17661805+Xenapte@users.noreply.github.com>
2022-03-07 23:11:13 +0800

    cache & refresh system

--- ballancemaps_fetch.py
+++ ballancemaps_fetch.py
@@ -6,26 +6,38 @@ from re import findall, search
 username = "ballancemaps"
 index_link = f"http://cc.ysepan.com/f_ht/ajcx/ml.aspx?cz=ml_dq&_dlmc={username}&_dlmm="
 filelist_link = f"http://cc.ysepan.com/f_ht/ajcx/wj.aspx?cz=dq&jsq=0&mlbh=%s&wjpx=1&_dlmc={username}&_dlmm="
+indexes = []
+map_list = {}
+
+
+def refresh():
+  global indexes, map_list
+  indexes = []
+  map_list = {}
 
 
 def get_group_indexes(pattern_str):
-  html_string = get_ys_html(index_link)
-  indexes = []
-  for matches in findall(
+  global indexes
+  if len(indexes) == 0:
+    html_string = get_ys_html(index_link)
+    indexes = list(map(
+      lambda matches: {
+        "id": matches[0],
+        "name": matches[1],
+        "notes": matches[2]
+      },
+      findall(
         r'<li[^<>]+id="ml_([0-9]+)"[^<>]*>.*?<a [^<>]*>([^<>]+)</a><label>([^<>]+)?</label>.*?</li>',
         html_string
-      ):
-    if search(pattern_str, matches[1]):
-      indexes.append(matches)
+      )
+    ))
 
-  return list(map(
-    lambda matches: {
-      "id": matches[0],
-      "name": matches[1],
-      "notes": matches[2]
-    },
-    indexes
-  ))
+  matched_indexes = []
+  for index in indexes:
+    if search(pattern_str, index["id"]):
+      matched_indexes.append(index)
+
+  return matched_indexes
 
 
 def get_map_list(index):
@@ -34,22 +46,26 @@ def get_map_list(index):
     if count == 0: count = -1
     return count
 
-  html_string = get_ys_html(filelist_link % index)
-  return list(map(
-    lambda matches: {
-      "name": matches[1],
-      "url": matches[0],
-      "size": matches[2],
-      "author": matches[3],
-      "difficulty": difficulty_to_number(matches[4]),
-      "notes": matches[5],
-      "upload_time": matches[6]
-    },
-    findall(
-      r'<li(?:[^<>]+)>.*?<a[^<>]+?href="([^">]+)"(?:[^<>]+)?>([^<>]+)<\/a><i>([^<]+)<\/i><b>\s*([^\s|<>]+(?:\s+[^\s|<>]+)*)?\s*\|?\s*([^\s|<>]+(?:\s+[^\s|<>]+)*)?\s*\|?\s*([^\s|<>]+(?:\s+[^\s|<>]+)*)?\s*<\/b><span(?:[^<>]+)>([^<>]+)<\/span>.*?<\/a><\/li>',
-      html_string
-    )
-  ))
+  global map_list
+  if index not in map_list or len(index) == 0:
+    html_string = get_ys_html(filelist_link % index)
+    map_list[index] = list(map(
+      lambda matches: {
+        "name": matches[1],
+        "url": matches[0],
+        "size": matches[2],
+        "author": matches[3],
+        "difficulty": difficulty_to_number(matches[4]),
+        "notes": matches[5],
+        "upload_time": matches[6]
+      },
+      findall(
+        r'<li(?:[^<>]+)>.*?<a[^<>]+?href="([^">]+)"(?:[^<>]+)?>([^<>]+)<\/a><i>([^<]+)<\/i><b>\s*([^\s|<>]+(?:\s+[^\s|<>]+)*)?\s*\|?\s*([^\s|<>]+(?:\s+[^\s|<>]+)*)?\s*\|?\s*([^\s|<>]+(?:\s+[^\s|<>]+)*)?\s*<\/b><span(?:[^<>]+)>([^<>]+)<\/span>.*?<\/a><\/li>',
+        html_string
+      )
+    ))
+  
+  return map_list[index]
 
 
 def get_ys_html(url):
